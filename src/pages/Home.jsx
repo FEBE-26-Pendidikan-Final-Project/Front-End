309636d546631193036ae0461386147c9ae0fcb9
salimsulaiman <salimsulaiman75@gmail.com>
2022-11-24 16:26:08 +0700

    membuat modal join class

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,8 +9,6 @@ function Home() {
     if (localStorage.getItem("idUser") === null) {
       navigate("/");
     }
-
-
   });
   return (
     <>
@@ -26,9 +24,39 @@ function Home() {
             </form>
           </div>
           <div className="col-lg-2 my-2 d-grid">
-            <button type="button" className="btn" id="btn-join">
+            <button type="button" className="btn" id="btn-join" data-bs-toggle="modal" data-bs-target="#joinClass">
               Join Class
             </button>
+            <div className="modal fade" id="joinClass" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+              <div className="modal-dialog">
+                <div className="modal-content">
+                  <div className="modal-header">
+                    <h1 className="modal-title fs-5" id="exampleModalLabel">
+                      Join Class
+                    </h1>
+                    <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                  </div>
+                  <form>
+                    <div className="modal-body">
+                      <div className="mb-3">
+                        <label for="exampleInputEmail1" className="form-label">
+                          Please input token class
+                        </label>
+                        <input type="text" className="form-control" id="tokenclass" />
+                      </div>
+                    </div>
+                    <div className="modal-footer">
+                      <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">
+                        Close
+                      </button>
+                      <button type="button" className="btn btn-primary">
+                        Save changes
+                      </button>
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
